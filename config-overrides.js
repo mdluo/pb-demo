bb1975a058ee2c224f20676ea37f3a046a9e2a2f
Mingdong Luo <mdluo@live.com>
2018-06-08 16:32:22 +0800

    Add antd

---
+++ config-overrides.js
@@ -0,0 +1,48 @@
+const { injectBabelPlugin, getLoader, loaderNameMatches, compose } = require('react-app-rewired');
+const paths = require('react-app-rewired/scripts/utils/paths');
+const rewireLess = require('react-app-rewire-less-modules');
+const rewireVendorSplitting = require('react-app-rewire-vendor-splitting');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+/* eslint no-param-reassign: [0] */
+module.exports = function override(config, env) {
+  config = injectBabelPlugin([
+    'import',
+    { libraryName: 'antd', style: true, libraryDirectory: 'es' },
+  ], config);
+
+  if (env === 'production') {
+    config.entry = { main: paths.appIndexJs };
+
+    // Change css module class names in production
+    const cssLoader = getLoader(
+      config.module.rules,
+      rule => String(rule.test) === String(/\.module\.css$/),
+    ).loader.find(loader => loaderNameMatches(loader, 'css-loader'));
+    cssLoader.options.localIdentName = 'pb-[hash:base64:8]';
+
+    // Include bundle analyzation
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+      }),
+    );
+  }
+
+  config = compose(
+    rewireLess.withLoaderOptions({
+      modifyVars: {
+        '@primary-color': '#2f54eb',
+        '@icon-url': '"/static/font/iconfont-3.x/iconfont"',
+        '@font-size-base': '16px',
+        '@font-size-sm': '14px',
+        '@font-family': '"Roboto", sans-serif;',
+      },
+      javascriptEnabled: true,
+    }),
+    rewireVendorSplitting,
+  )(config, env);
+
+  return config;
+};
---
+++ src/vendors.js
@@ -0,0 +1,6 @@
+module.exports = {
+  vendors: [
+    'react',
+    'react-dom',
+  ],
+};
